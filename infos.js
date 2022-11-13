fce22a1a9414dd05e8d224647a43e5a536938329
fatma-laribi <fatmalaribi@insat.u-carthage.tn>
2022-11-13 19:01:36 +0100

    info page added

---
+++ infos.js
@@ -0,0 +1,67 @@
+function rechercher(researchedPlanet) {
+    var lien_infos = "<http://dbpedia.org/resource/"+researchedPlanet+">";
+    var contenu_requete = `PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    PREFIX dc: <http://purl.org/dc/elements/1.1/>
+    PREFIX : <http://dbpedia.org/resource/>
+    PREFIX dbpedia2: <http://dbpedia.org/property/>
+    PREFIX dbpedia: <http://dbpedia.org/>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+    SELECT ?property ?hasValue ?isValueOf
+    WHERE {
+    { `+lien_infos +` ?property ?hasValue }
+    UNION
+    { ?isValueOf ?property`+lien_infos +` }
+    }`;
+
+    // Encodage de l'URL à transmettre à DBPedia
+    var url_base = "http://dbpedia.org/sparql";
+    var url = url_base + "?query=" + encodeURIComponent(contenu_requete) + "&format=json";
+
+    // Requête HTTP et affichage des résultats
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var results = JSON.parse(this.responseText);
+            console.log(results)
+            afficherResultats(results);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+async function chercherImages(researchedPlanet){
+    var getJSON = function(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'json';
+        xhr.onload = function() {
+        var status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
+        };
+        xhr.send();
+    };
+    var url = "https://en.wikipedia.org/w/api.php?action=query&titles="+researchedPlanet+"&prop=pageimages&format=json&origin=*";
+    getJSON(url,function(err, data) {
+    if (err !== null) {
+        alert('Something went wrong: ' + err);
+    }
+    else {
+        const stringifiedJson = JSON.stringify(data);
+        var source = stringifiedJson.split("source\":\"")[1];
+        source = source.split("\"}}}}")[0];
+        document.getElementById("planetImage").setAttribute("src",source);
+    }
+
+
+    });
+
+}
