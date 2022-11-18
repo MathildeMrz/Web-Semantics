aa15d216f58fa1592f1dd9c59cd58d1c24246f87
MathildeMrz <mathilde.marza2001@gmail.com>
2022-11-18 10:31:26 +0100

    reparation

--- Interface/Javascript/research-page.js
+++ Interface/Javascript/research-page.js
@@ -1,8 +1,7 @@
-
-  function rechercher(deity, language) {
-    //console.log("deity : "+deity);
-    //console.log("language : "+language);
-    var searchedPlanet = document.getElementById("search").value;
-    //console.log("searchedPlanet : "+searchedPlanet);
-    window.location.replace("../Src/search-page.html?"+searchedPlanet+"?"+language+"?"+deity);
-  }
+function rechercher(deity, language) {
+  //console.log("deity : "+deity);
+  //console.log("language : "+language);
+  var searchedPlanet = document.getElementById("search").value;
+  //console.log("searchedPlanet : "+searchedPlanet);
+  window.location.replace("../Src/search-page.html?"+searchedPlanet+"?"+language+"?"+deity);
+}
